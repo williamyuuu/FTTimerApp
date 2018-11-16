fd5458987183ebe2078c6ee87dc2e9febd25f7a9
William Yu <williamyu@Williams-MacBook-Air.local>
2018-11-15 18:23:23 -0800

    Working one minute timer turns yellow at 15 seconds, red at 0 constantly runs even upon reaching 0

--- FTTimer.java
+++ FTTimer.java
@@ -3,18 +3,41 @@
 import java.awt.event.*; //allows for ActionListener
 import java.awt.*;
 import javax.swing.*;
+import java.util.TimerTask;
+import java.util.Timer;
 
-public class FTTimer extends JFrame{
+public class tester extends JFrame{
+
+	JPanel panel = new JPanel();
+
+	Timer timer = new Timer();
+	TimerTask task = new TimerTask() {
+		public void run() {
+			secondsPassed--;
+			displayMin = secondsPassed / 60;
+			displaySec = secondsPassed % 60;
+			System.out.println(secondsPassed); //displays on cmd
+			updateCounter();
+			if(secondsPassed < 1){
+				secondsPassed = 1;
+				panel.setBackground(Color.RED);
+			} else if(secondsPassed <= RESET_TIME*0.25) { //75% is at 15 seconds
+				panel.setBackground(Color.YELLOW);
+			}
+		}
+	};
 
 	private JButton buttonCounter, buttonReset;
 	private JLabel labelCount;
+	private final int RESET_TIME = 60;
+	private int displayMin = RESET_TIME/60;
+	private int displaySec = RESET_TIME%60;
+	private int secondsPassed;
 
-	private int clicks = 0;
-
-	public FTTimer() {
+	public tester() {
 		createView();
 
-		setTitle("Click Me");
+		setTitle("FTTimer version a.1");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		pack();
 		setLocationRelativeTo(null);
@@ -22,50 +45,54 @@ public class FTTimer extends JFrame{
 	}
 
 	private void createView(){
-		JPanel panel = new JPanel();
+		//JPanel panel = new JPanel();
 		getContentPane().add(panel);
+		panel.setOpaque(true);
 
 		labelCount = new JLabel();
 		labelCount.setPreferredSize(new Dimension(200, 30));
 		panel.add(labelCount);
 		updateCounter();
 
-		buttonCounter = new JButton("Click me");
+		buttonCounter = new JButton("Start Timer");
 		buttonCounter.addActionListener(
 			new ButtonCounterActionListener());
 		panel.add(buttonCounter);
 
-		buttonReset = new JButton("Reset");
+
+		buttonReset = new JButton("Stop Timer");
 		buttonReset.addActionListener(
 			new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					clicks = 0;
+					secondsPassed = 0;
 					updateCounter();
+					timer.cancel();
+					System.out.println("Timer task completely wiped. GG noob!");
 				}
 			}
 		);
 		panel.add(buttonReset);
-
 	}
 
 	private void updateCounter(){
-		labelCount.setText("Clicked " + clicks + " times");
+		labelCount.setText(String.format("%02d:%02d",displayMin,displaySec));
 	}
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable(){
 			@Override
 			public void run(){
-				new FTTimer().setVisible(true);
+				new tester().setVisible(true);
 			}
 		});
 	}
 	private class ButtonCounterActionListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			clicks++;
-			updateCounter();
+			panel.setBackground(Color.GREEN);
+			secondsPassed = RESET_TIME;
+			timer.scheduleAtFixedRate(task,1000,1000);
 		}
 	}
 }
