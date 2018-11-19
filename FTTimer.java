e840b5ca5334c53de79eebe3cd36bac3ebd6df46
William Yu <williamyu@Williams-MacBook-Air.local>
2018-11-19 03:16:37 -0800

    timer.cancel fix Timer stops at zero Only one timer runs at a time Timer can be created after stopping timer

--- FTTimer.java
+++ FTTimer.java
@@ -10,34 +10,21 @@ public class FTTimer extends JFrame{
 
 	JPanel panel = new JPanel();
 
-	Timer timer = new Timer();
-	TimerTask task = new TimerTask() {
-		public void run() {
-			secondsPassed--;
-			displayMin = secondsPassed / 60;
-			displaySec = secondsPassed % 60;
-			System.out.println(secondsPassed); //displays on cmd
-			updateCounter();
-			if(secondsPassed < 1){
-				secondsPassed = 1;
-				panel.setBackground(Color.RED);
-			} else if(secondsPassed <= RESET_TIME*0.25) { //75% is at 15 seconds
-				panel.setBackground(Color.YELLOW);
-			}
-		}
-	};
-
-	private JButton buttonCounter, buttonReset;
+	private JButton buttonStart, buttonPause;
 	private JLabel labelCount;
 	private final int RESET_TIME = 60;
 	private int displayMin = RESET_TIME/60;
 	private int displaySec = RESET_TIME%60;
 	private int secondsPassed;
+	private int timerRunning = 0;
+
+	Timer timer;
+	TimerTask task;
 
 	public FTTimer() {
 		createView();
 
-		setTitle("FTTimer version a.1");
+		setTitle("Frenzy Timer version a.1");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		pack();
 		setLocationRelativeTo(null);
@@ -45,38 +32,78 @@ public class FTTimer extends JFrame{
 	}
 
 	private void createView(){
-		//JPanel panel = new JPanel();
+		//JPanel panel = new JPanel(); //blocks colors from showing (?)
 		getContentPane().add(panel);
 		panel.setOpaque(true);
+		panel.setBackground(Color.GREEN);
 
 		labelCount = new JLabel();
 		labelCount.setPreferredSize(new Dimension(200, 30));
 		panel.add(labelCount);
 		updateCounter();
 
-		buttonCounter = new JButton("Start Timer");
-		buttonCounter.addActionListener(
-			new ButtonCounterActionListener());
-		panel.add(buttonCounter);
+		//start 10 minute timer
+		buttonStart = new JButton("Start Timer");
+		buttonStart.addActionListener(
+			new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					secondsPassed = RESET_TIME;
+					startTime();
+			}
+		});
+		panel.add(buttonStart);
 
-
-		buttonReset = new JButton("Stop Timer");
-		buttonReset.addActionListener(
+		//holds the time and cancels the timer
+		buttonPause = new JButton("Stop Timer");
+		buttonPause.addActionListener(
 			new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					secondsPassed = 0;
+					stopTime();
 					updateCounter();
-					timer.cancel();
-					System.out.println("Timer task completely wiped. GG noob!");
+					System.out.println("Timer stopped!");
 				}
 			}
 		);
-		panel.add(buttonReset);
+		panel.add(buttonPause);
 	}
 
 	private void updateCounter(){
 		labelCount.setText(String.format("%02d:%02d",displayMin,displaySec));
+	}
+
+	private void startTime() {
+		if(timer != null) {
+			stopTime();
+		}
+			timer = new Timer();
+			createTask();
+			timer.scheduleAtFixedRate(task,1000,1000);
+	}
+
+	private void createTask() {
+		task = new TimerTask() {
+		   public void run() {
+			   secondsPassed--;
+			   displayMin = secondsPassed / 60;
+			   displaySec = secondsPassed % 60;
+			   System.out.println(secondsPassed); //displays on cmd
+			   updateCounter();
+			   if(secondsPassed < 1){
+				   secondsPassed = 1;
+				   stopTime();
+				   panel.setBackground(Color.RED);
+			   } else if(secondsPassed <= RESET_TIME*0.25) { //75% is at 15 seconds
+				   panel.setBackground(Color.YELLOW);
+			   }
+		   } //end of run
+	   }; //end of timer task
+	}
+
+	private void stopTime() {
+		timer.cancel();
 	}
 
 	public static void main(String[] args) {
@@ -86,13 +113,5 @@ public class FTTimer extends JFrame{
 				new FTTimer().setVisible(true);
 			}
 		});
-	}
-	private class ButtonCounterActionListener implements ActionListener {
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			panel.setBackground(Color.GREEN);
-			secondsPassed = RESET_TIME;
-			timer.scheduleAtFixedRate(task,1000,1000);
-		}
 	}
 }
