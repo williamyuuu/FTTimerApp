7466c38167512a7fae61e46986aef4a17f55c765
William Yu <williamyu@Williams-MacBook-Air.local>
2018-12-13 12:34:39 -0800

    Typing into Map textfield pops up suggestions

--- SearchMapHandler.java
+++ SearchMapHandler.java
@@ -4,15 +4,33 @@ import javax.swing.*;
 
 public class SearchMapHandler implements KeyListener{
 
-    JTextField textfield, textSuper;
-    JComboBox comboBox;
+    //List of names
+    //** Get a Reader to read a database file of all map
+    //** input the names into the mapList
+    private static String[] mapList = {"Cavern Lower Path", "CLP", "Chicken Festival 1",
+                                        "Chicken Festival 2", "Chicken Festival 3"};
 
-    //read a .db file
-    //store .db data into an array
-    //if data within array matches the data that the user types, send out combobox
-    //suggestions will be displayed
+    private JTextField textfield;
+    private JComboBox<String> comboBox;
 
-    public void keyPressed(KeyEvent e){}
+    //When a key is typed, it will open up combobox with maplist suggestions
+    // ** still need to get the position to display properly
+    // ** possibly only start suggestion after 2 letters?
+    public void keyPressed(KeyEvent e){
+     textfield = (JTextField) e.getComponent();
+        comboBox = new JComboBox<String>();
+        comboBox.setFocusCycleRoot(true);
+        comboBox.setFocusTraversalPolicyProvider(true);
+        comboBox.setAutoscrolls(true);
+        comboBox.setBorder(null);
+        comboBox.setOpaque(false);
+        comboBox.setBounds(25, 19, 268, 37);
+        for(String string : mapList) {
+            textfield.add(comboBox);
+            comboBox.addItem(string);
+            comboBox.showPopup();
+        }
+    }
     public void keyReleased(KeyEvent e){}
     public void keyTyped(KeyEvent e){}
 
