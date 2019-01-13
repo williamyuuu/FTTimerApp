425e312565bcab357c9f88f0cf37c52cfab51e9c
William Yu <williamyu@Williams-MacBook-Air.local>
2019-01-12 22:15:43 -0800

    deleted old debugger

--- SearchHandler.java
+++ SearchHandler.java
@@ -93,12 +93,10 @@ public class SearchMapHandler extends JFrame implements KeyListener,ActionListen
     private void runOnce (JTextField textfield){
         //DBReader code. Opens the map database and returns the array.
         if(textfield.getText().equals("Map Name ")){
-            System.out.println(textfield.getText() + "---------");
             System.out.println("running mapnames.db");
             mapList = database.getArray("mapnames.db");
         }
         else if(textfield.getText().equals("Name ")){
-            System.out.println(textfield.getText() + "----------");
             System.out.println("running username.db");
             mapList = database.getArray("usernames.db");
         }
